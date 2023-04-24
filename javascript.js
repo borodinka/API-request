7000a5853625d90c51a1be388177defe83d321b7
borodinka <95708108+borodinka@users.noreply.github.com>
2023-04-24 12:26:21 +0200

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,42 @@
+const API_URL = 'https://dog.ceo/api';
+
+async function getDog() {
+	try {
+		const loadedDog1 = await fetch(`${API_URL}/breeds/image/random`);
+		const parsedDog1 = await loadedDog1.json();
+
+		const loadedDog2 = await fetch(`${API_URL}/breeds/list/all`);
+		const parsedDog2 = await loadedDog2.json();
+
+		const loadedDog3 = await fetch(`${API_URL}/breed/dalmatian/images/random`);
+		const parsedDog3 = await loadedDog3.json();
+
+		return [parsedDog1, parsedDog2, parsedDog3];
+	} catch (error) {
+		console.log('Error: ', error);
+		throw new Error('Error fetching dog data.');
+	}
+}
+
+async function showData() {
+	try {
+		const [randomImage, breeds, dalmatianImage] = await getDog();
+
+		const randomImg = document.getElementById('random-image');
+		randomImg.src = randomImage.message;
+
+		const breedList = document.getElementById('breeds');
+		for (const breed in breeds.message) {
+			const breedItem = document.createElement('li');
+			breedItem.textContent = breed;
+			breedList.appendChild(breedItem);
+		}
+
+		const dalmatianImg = document.getElementById('dalmatian-image');
+		dalmatianImg.src = dalmatianImage.message;
+	} catch (error) {
+		console.log('Error: ', error);
+	}
+}
+
+showData();
