1b573e821641d735f5bc16ec3819e60696112047
borodinka <95708108+borodinka@users.noreply.github.com>
2023-04-25 21:33:10 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,19 +2,29 @@ const API_URL = 'https://dog.ceo/api';
 
 async function getDog() {
 	try {
-		const loadedDog1 = await fetch(`${API_URL}/breeds/image/random`);
-		const parsedDog1 = await loadedDog1.json();
+		const data = await Promise.all([
+			fetch(`${API_URL}/breeds/image/random`),
+			fetch(`${API_URL}/breeds/list/all`),
+			fetch(`${API_URL}/breed/dalmatian/images/random`)
+		]);
 
-		const loadedDog2 = await fetch(`${API_URL}/breeds/list/all`);
-		const parsedDog2 = await loadedDog2.json();
+		const results = await Promise.all(data.map(a => a.json()));
 
-		const loadedDog3 = await fetch(`${API_URL}/breed/dalmatian/images/random`);
-		const parsedDog3 = await loadedDog3.json();
+		return results;
 
-		return [parsedDog1, parsedDog2, parsedDog3];
+		// const loadedDog1 = await fetch(`${API_URL}/breeds/image/random`);
+		// const parsedDog1 = await loadedDog1.json();
+
+		// const loadedDog2 = await fetch(`${API_URL}/breeds/list/all`);
+		// const parsedDog2 = await loadedDog2.json();
+
+		// const loadedDog3 = await fetch(`${API_URL}/breed/dalmatian/images/random`);
+		// const parsedDog3 = await loadedDog3.json();
+
+		// return [parsedDog1, parsedDog2, parsedDog3];
 	} catch (error) {
 		console.log('Error: ', error);
-		throw new Error('Error fetching dog data.');
+		throw new Error('Error fetching dog data');
 	}
 }
 
